55f5606324f94012d0d5b305570d6e7925d4bcd1
Apoorv Varshney <85228469+apoorv-5369@users.noreply.github.com>
2022-01-31 23:59:45 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m=matrix.length, n=matrix[0].length;
+        for(int i=0; i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==target) return true;
+            }
+        }
+        return false;
+    }
+}
